b4037ecc71e50f40fe999e2db7e656751761984e
Maciek Pekala <m@peka.la>
2019-08-27 14:00:58 +0200

    Some logging

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
+    const myInput = core.getInput("myInput");
     core.debug(`Hello ${myInput}`);
+    console.log(`Hello ${myInput}`);
   } catch (error) {
     core.setFailed(error.message);
   }
