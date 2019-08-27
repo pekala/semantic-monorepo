37d8183228fa67b89ad1e4151720965466fcf4ba
Maciek Pekala <m@peka.la>
2019-08-27 15:29:32 +0200

    Show affected files

--- src/walk-commits.ts
+++ src/walk-commits.ts
@@ -1,5 +1,6 @@
 import { plugins, applyPlugins, parse } from "parse-commit-message";
 import getRawCommit from "./get-raw-commit";
+import getAffectedFiles from "./get-affected-files";
 
 async function parseCommit(rawMessage: string) {
   try {
@@ -16,8 +17,9 @@ export default async function* walkCommits() {
     try {
       const { hash, rawMessage } = await getRawCommit(cursor);
       const commit = await parseCommit(rawMessage);
+      const affectedFiles = await getAffectedFiles(hash);
 
-      yield { hash, commit };
+      yield { hash, commit, affectedFiles };
       cursor = `${hash}^1`;
     } catch (error) {
       if (error.exitCode === 128) {
