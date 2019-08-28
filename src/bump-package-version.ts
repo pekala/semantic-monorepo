856d542493d812bc9c2720cf2f304ac314037bfd
Maciek Pekala <m@peka.la>
2019-08-28 11:39:24 +0200

    Add git credentials

--- src/bump-package-version.ts
+++ src/bump-package-version.ts
@@ -17,7 +17,11 @@ export default async function bumpPkgVersion(
     cwd,
     env: {
       YARN_VERSION_GIT_MESSAGE: `chore(release): ${pkgName}@v%s`,
-      YARN_VERSION_TAG_PREFIX: `${pkgName}@v`
+      YARN_VERSION_TAG_PREFIX: `${pkgName}@v`,
+      GIT_AUTHOR_NAME: "Semantic Monorepo",
+      GIT_COMMITTER_NAME: process.env.GITHUB_ACTOR,
+      GIT_AUTHOR_EMAIL: "dev@pleo.io",
+      GIT_COMMITTER_EMAIL: "dev@pleo.io"
     }
   });
   await execa("yarn", ["publish", `--non-interactive`], { cwd });
